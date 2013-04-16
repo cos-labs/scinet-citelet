0c309e7527cd07231f9ac61818ab56d954a590b5
Joshua Carp <jm.carp@gmail.com>
2013-04-15 21:12:40 -0400

    modified to wrap multiple files and run from CLI

--- minify.py
+++ minify.py
@@ -1,17 +1,31 @@
 # Imports
 import slimit
 
+code_names = [
+    'publisherdetector',
+    'headextractor',
+    'refextractor',
+    'citelet',
+]
+
 def minicite():
+    '''Concatenate, wrap, and minify project JavaScript files.'''
     
     # Read code
-    with open('static/js/citelet.js') as code_file:
-        code = code_file.read()
+    code = []
+    for code_name in code_names:
+        with open('static/js/%s.js' % (code_name)) as code_file:
+            code.append(code_file.read())
+    
+    # Concatenate code
+    code = '\n'.join(code)
 
     # Read wrapper
     with open('static/js/wrapper.js') as wrap_file:
         wrap = wrap_file.read()
     
     # Wrap code
+    # Note: Wrapper file must contain %s for interpolation
     wrapped_code = wrap % code
 
     # Minify wrapped code
@@ -20,3 +34,7 @@ def minicite():
     # Write minified code
     with open('static/js/citelet.min.js', 'w') as min_file:
         min_file.write(mini_code)
+
+# Run if called from CLI
+if __name__ == '__main__':
+    minicite()
