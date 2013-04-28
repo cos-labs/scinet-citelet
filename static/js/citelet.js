f101b596969867044afb1571df425c1069a30c88
Joshua Carp <jm.carp@gmail.com>
2013-04-28 15:56:01 -0400

    reorganization / comments

--- static/js/citelet.js
+++ static/js/citelet.js
@@ -1,28 +1,27 @@
-/*
-    Resolve potential conflict in JSON.stringify with Prototype.js; see
-    http://stackoverflow.com/questions/710586/json-stringify-bizarreness
-*/
+/**
+ * Resolve potential conflict in JSON.stringify with Prototype.js; see
+ * http://stackoverflow.com/questions/710586/json-stringify-bizarreness
+ */
 var stringify = Object.toJSON ? Object.toJSON : JSON.stringify;
 
-/* Get page data */
-var publisher = PublisherDetector.detect();
-var head_ref = stringify(HeadExtractor.extract(publisher));
-var cited_refs = stringify(RefExtractor.extract(publisher));
+// Build data dictionary
+var data = {};
+data['url'] = window.location.href;
+data['publisher'] = PublisherDetector.detect();
+if (data['publisher'] !== '') {
+    data['head_ref'] = stringify(HeadExtractor.extract(publisher));
+    data['cited_refs'] = stringify(RefExtractor.extract(publisher));
+}
 
-/*
-Send data to server
-    Note: Using JSONP instead of JSON to allow cross-domain calls
-*/
+/**
+ * Send data to server
+ *     Note: Using JSONP instead of JSON to allow cross-domain calls
+ */
 $.ajax({
     url : 'http://127.0.0.1:5000/sendrefs/',
     dataType : 'jsonp',
-    async : false,
-    data : {
-        publisher : publisher,
-        head_ref : head_ref,
-        cited_refs : cited_refs,
-    },
+    data : data,
     success : function(res) {
-        alert(res['msg']);
+        console.log(res['msg']);
     },
 });
