b678a4df550449e3d976595f89d7b76490344385
Joshua Carp <jm.carp@gmail.com>
2013-05-13 09:56:45 -0400

    added status, clear

--- ext/options.js
+++ ext/options.js
@@ -1,9 +1,20 @@
 // Initialize mode <select> to currently stored value
-var mode = chrome.storage.local.get('mode', function(result) {
+chrome.storage.local.get('mode', function(result) {
     $('#mode').val(result.mode);
 });
 
 // Save mode in Chrome storage
 $('#submit').click(function() {
     chrome.storage.local.set({mode : $('#mode').val()});
+});
+
+// 
+$('#clear').click(function() {
+    chrome.storage.local.clear();
+});
+
+// Show progress
+chrome.storage.local.get('sent', function(stored) {
+    var count = 'sent' in stored ? Object.keys(stored.sent).length : 0;
+    $('#progress').text(count + ' articles contributed so far.');
 });
