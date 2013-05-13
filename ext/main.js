224eee5498e3db5b3760aba6ab0b7e4f5484ae27
Joshua Carp <jm.carp@gmail.com>
2013-05-13 09:57:51 -0400

    run at window.onload

--- ext/main.js
+++ ext/main.js
@@ -1,1 +1,6 @@
-citelet_ext.ext();
+// Note: Fire extension on window load (rather than document ready)
+// to avoid complications with Oxford journals, which hide parts of
+// JQueryUI elements on window load.
+$(window).load(function(){
+    citelet_ext.ext()
+});
