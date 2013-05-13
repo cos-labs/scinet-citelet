f9961fba66bd5f084fb74556ceb77bb5d8cd47c9
Joshua Carp <jm.carp@gmail.com>
2013-05-13 09:57:21 -0400

    reorganized as separate functions joined by pipes

--- ext/citelet.ext.js
+++ ext/citelet.ext.js
@@ -1,150 +1,172 @@
 /* 
  * @module citelet_ext
  */
-var citelet_ext = (function(citelet) {
+var citelet_ext = (function() {
+    
+    function init() {
+        var defer = $.Deferred();
+        var state = {};
+        chrome.storage.local.get('mode', function(stored) {
+            if (typeof(stored.mode) === 'undefined') {
+                var stored = {mode : 'confirm'};
+                chrome.storage.local.set(stored);
+            }
+            switch(stored.mode) {
+                case 'noconfirm':
+                    state.doconfirm = false;
+                    defer.resolve(state);
+                    break;
+                case 'confirm':
+                    state.doconfirm = true;
+                    defer.resolve(state);
+                    break;
+                default:
+                    return $.Deferred().reject();
+                    break;
+            }
+        });
+        return defer;
+    }
+    
+    function doconfirm_to_scrape(state) {
+        var defer = $.Deferred();
+        citelet.scrape().done(function(data) {
+            state.data = data;
+            defer.resolve(state);
+        });
+        return defer;
+    }
+    
+    function scrape_to_valid(state) {
+        // Skip if publisher not found
+        if (state.data['publisher'] === '') {
+            // Break chain by returning a rejected deferred
+            return $.Deferred().reject();
+        }
+        return state;
+    }
+        
+    /* 
+     * Check for reference in Chrome storage. Adds
+     * 'sent' field to state.
+     * 
+     * @class valid_to_stored
+     * @static
+     * @param state {Object} Extension state so far
+     * @param state.data {Object} Scraped data
+     */
+    function valid_to_stored(state) {
+        var defer = $.Deferred();
+        chrome.storage.local.get('sent', function(storage) {
+            var _sent = 'sent' in storage ? storage.sent : {};
+            state.sent = typeof(_sent[state.data['url']]) === 'undefined';
+            defer.resolve(state);
+        });
+        return defer;
+    }
     
     /* 
+     * Check the sent field and get user confirmation. Adds
+     * 'confirmed' field to state.
+     * 
+     * @class confirmed_to_send
+     * @static
+     * @param state {Object} Extension state so far
+     * @param state.data {Object} Scraped data
+     * @param state.confirmed {Boolean} Send confirmed?
+     */
+    function stored_to_confirmed(state) {
+        if (state.sent) {
+            var defer = $.Deferred();
+            var confirm_defer = state.doconfirm ?
+                                ext_confirm.confirm_jq() : 
+                                $.when(true);
+            confirm_defer.done(function(confirmed) {
+                state.confirmed = confirmed;
+                defer.resolve(state);
+            });
+            return defer;
+        } else {
+            console.log('References already sent. Quitting...');
+            // Break chain by returning a rejected deferred
+            return $.Deferred().reject();
+        }
+    }
+    
+    /* 
+     * Process confirmation information and send references to server. Adds
+     * 'res' field to state.
+     * 
+     * @class confirmed_to_send
+     * @static
+     * @param state {Object} Extension state so far
+     * @param state.data {Object} Scraped data
+     * @param state.confirmed {Boolean} Send confirmed?
+     */
+    function confirmed_to_send(state) {
+        var defer = $.Deferred();
+        if (state.confirmed) {
+            console.log('Sending references...');
+            citelet.send(state.data, {
+                dataType : 'json',
+                success : function(res) {
+                    state.res = res;
+                    defer.resolve(state);
+                }
+            });
+        } else {
+            console.log('Not confirmed. Quitting...');
+            // Break chain by returning a rejected deferred
+            return $.Deferred().reject();
+        }
+        return defer;
+    }
+    
+    /* 
+     * Process JSON response from server after sending references.
+     * 
+     * @class send_to_store
+     * @static
+     * @param state {Object} Extension state so far
+     * @param state.res {Object} JSON response
+     * @param state.data {Object} Scraped data
+     */
+    function send_to_store(state) {
+        if (state.res.status == 'success') {
+            console.log('Storing references in Chrome...');
+            chrome.storage.local.get('sent', function(storage) {
+                var sent = 'sent' in storage ? storage.sent : {};
+                sent[state.data['url']] = state.data;
+                chrome.storage.local.set({'sent' : sent});
+            });
+        }
+    }
+    
+
+    /* 
+     * Main method for the Citelet Chrome extension. Chains together the steps
+     * involved in fetching article meta-data and sending them to the Citelet 
+     * server.
+     * 
      * @class ext
      * @static
      */
     function ext() {
         
-        chrome.storage.local.get('mode', function(items) {
-            if (typeof(items.mode) === 'undefined') {
-                var items = {mode : 'confirm'};
-                chrome.storage.local.set(items);
-            }
-            switch(items.mode) {
-                case 'noconfirm':
-                    ext_inner(false);
-                    break;
-                case 'confirm':
-                    ext_inner(true);
-                    break;
-            }
-        });
-
+        init()
+            .then(doconfirm_to_scrape)
+            .then(scrape_to_valid)
+            .then(valid_to_stored)
+            .then(stored_to_confirmed)
+            .then(confirmed_to_send)
+            .done(send_to_store);
+            
     }
     
-    function ext_confirm() {
-        return $.when(confirm('Send references to Citelet?'));
-    }
-    
-    function ext_confirm_jq() {
-        
-        var container_id = 'citelet-container';
-        var dialog_id = 'citelet-dialog';
-        
-        // Create dialog <div> if it doesn't exist
-        if ($('#' + dialog_id).length == 0) {
-            
-            // Create container
-            var container = document.createElement('div');
-            container.setAttribute('id', container_id);
-            
-            // Create dialog
-            var dialog = document.createElement('div');
-            dialog.setAttribute('id', dialog_id);
-            dialog.setAttribute('title', 'Confirmation');
-            dialog.innerText = 'Send references to Citelet?';
-            
-            // Append dialog to container
-            container.appendChild(dialog);
-            
-            // Append container to body
-            document.body.appendChild(container);
-            
-        }
-        
-        // Create deferred object
-        var defer = $.Deferred();
-        
-        // Show modal dialog
-        $('#' + dialog_id).dialog({
-            modal : true,
-            appendTo : '#' + container_id,
-            buttons : {
-                Yes : function() {
-                    $(this).dialog('close');
-                    defer.resolve(true);
-                },
-                No : function() {
-                    $(this).dialog('close');
-                    defer.resolve(false);
-                },
-            },
-        });
-        
-        // Return deferred object
-        return defer;
-        
-    }
-    
-    /* 
-     * @class ext_inner
-     * @static
-     * @param do_confirm {Boolean} Confirm before sending?
-     */
-    function ext_inner(do_confirm) {
-        
-        // Get reference information from page
-        var defer = citelet.scrape();
-        
-        // Call ext_send() when done
-        defer.done(function(data) {
-            //ext_send(data, do_confirm)
-            
-            // Skip if publisher not found
-            if (data['publisher'] === '') {
-                return;
-            }
-            
-            var chain = $.Deferred();
-            chrome.storage.local.get(data['url'], function(items) {
-                chain.resolve(items);
-            });
-    
-            chain.pipe(function(items) {
-                if (typeof(items[data['url']]) === 'undefined') {
-                    return confirm_defer = do_confirm ?
-                                           ext_confirm_jq() : 
-                                           true
-                } else {
-                    console.log('References already sent. Quitting...');
-                    return false
-                }
-            }).pipe(function(confirmed) {
-                var defer = $.Deferred();
-                if (confirmed) {
-                    console.log('Sending references...');
-                    citelet.send(data, {
-                        dataType : 'json',
-                        success : function(res) {
-                            defer.resolve(res);
-                        }
-                    });
-                } else {
-                    console.log('Not confirmed. Quitting...');
-                    defer.resolve({});
-                }
-                return defer;
-            }).done(function(res) {
-                if (res.status == 'success') {
-                    console.log('Storing references in Chrome...');
-                    var storage = {};
-                    storage[data['url']] = data;
-                    chrome.storage.local.set(storage);
-                }
-            });
-    
-        });
-                    
-    }
-    
-    // Expose public fields
+    // Expose public methods & data
 
     return {
         ext : ext,
     };
 
-})(citelet);
+})();
