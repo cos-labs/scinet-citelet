4318b3ebbb7cb3aa97233612ad41a4e43ae0d1ee
Joshua Carp <jm.carp@gmail.com>
2013-05-13 09:59:31 -0400

    moved

--- static/js/bookmarklet.js
+++
@@ -1,5 +0,0 @@
-javascript:(function () { 
-	var jsCode = document.createElement('script'); 
-	jsCode.setAttribute('src', 'http://127.0.0.1:5000/static/js/citelet.min.js');      			
-    document.body.appendChild(jsCode); 
- }());
