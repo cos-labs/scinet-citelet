dd106510166fa8cd4aba5445f0b9c438fff5a901
Joshua Carp <jm.carp@gmail.com>
2013-05-22 15:36:16 -0400

    content

--- js-src/_citelet.js
+++ js-src/_citelet.js
@@ -71,7 +71,7 @@ var citelet = (function() {
         
         // Default options
         var opts = {
-            url : 'http://127.0.0.1:5000/sendrefs/',
+            url : 'http://citelet.herokuapp.com/sendrefs/',
             data : data,
             success : function(res) {
                 console.log(res['msg']);
