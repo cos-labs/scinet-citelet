fa92ca32c36d7559207b4e78b2484bb674ec7e7b
Joshua Carp <jm.carp@gmail.com>
2013-06-18 16:05:41 -0400

    deleted non-versioned js files

--- app/static/js/bookmarklet.js
+++
@@ -1,5 +0,0 @@
-javascript:(function () { 
-	var script = document.createElement('script'); 
-	script.setAttribute('src', 'http://localhost:5000/static/js/citelet.min.js');      			
-    document.body.appendChild(script); 
- }());
