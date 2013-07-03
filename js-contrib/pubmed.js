7cfbeb6aba034a3cfc2a9ec5f05d8155a1a5d59c
Joshua Carp <jm.carp@gmail.com>
2013-07-03 12:33:16 -0400

    account for different pmc formats

--- js-contrib/pubmed.js
+++ js-contrib/pubmed.js
@@ -11,20 +11,40 @@ new PublisherDetector.MetaPublisherDetector('pubmed', [
 new CitationExtractor.MetaCitationExtractor('pubmed');
 
 new ReferenceExtractor.ReferenceExtractor('pubmed', function () {
-    var refs_v1 = $('li[id^="B"]'),
-        refs_v2 = $('div.ref-cit-blk');
-    return refs_v1.length ? refs_v1 : refs_v2;
+
+    // List reference selectors
+    var selectors = [
+        'div#reference-list li[id^="R"]',
+        'div#reference-list li[id^="B"]',
+        'div#reference-list div.ref-cit-blk',
+    ];
+
+    // Loop over selectors until match
+    for (var selidx = 0; selidx < selectors.length; selidx++) {
+        var refs = $(selectors[selidx]);
+        if (refs.length)
+            return refs;
+    }
+
 });
 
 new ContactExtractor.ContactExtractor('pubmed', function() {
-    
-    var contact = {};
-    
+
+    var email = $('div.fm-authors-info a[href^="mailto:"]')
+        .map(ContactExtractor.clean_addr)
+        .get();
+
+    return {
+        email : email,
+    }
+
+    /*var contact = {};
+
     contact['email'] = $('ul.authorGroup img')        // Find <img> within authorGroup
         .parent('a')                                  // Find containing <a>
         .map(ContactExtractor.clean_addr)             // Clean email address
         .get();                                       // jQuery -> JavaScript
     
-    return contact;
+    return contact;*/
         
 });
