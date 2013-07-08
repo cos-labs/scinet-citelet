5a5ffb7de3ce41bec5df44e65009e5583bb8d98c
Joshua Carp <jm.carp@gmail.com>
2013-07-08 11:13:51 -0400

    added parameters

--- app/config.py
+++ app/config.py
@@ -4,13 +4,23 @@
 # Imports
 import os
 
-# 
+# Send mode 
+MODE = 'REMOTE'
+
+# Scholarly URL
+SCHOLAR_URL = 'http://50.16.225.235/raw'
+
+# MongoDB parameters
+
 DBNAME = 'citelet'
 COLLNAME = 'data'
 
 # Paths
+
+# Base path
 path = os.path.abspath(
     os.path.split(__file__)[0]
 )
 
+# Payload path
 payload_path = os.path.join(path, 'tests/payloads')
