890dd311e9bf7850d54467ef460cc768fa6cf11e
Joshua Carp <jm.carp@gmail.com>
2013-07-14 19:02:53 -0400

    ...

--- chrome-ext/main.js
+++ chrome-ext/main.js
@@ -1,6 +1,4 @@
-// Note: Fire extension on window load (rather than document ready)
-// to avoid complications with Oxford journals, which hide parts of
-// JQueryUI elements on window load.
-$(window).load(function(){
-    citelet_ext.ext()
+// Call main method of ext module on page load
+$(document).ready(function() {
+    ext.ext();
 });
