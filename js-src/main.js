b08bd4f064a89cad58c45ead1ae368b50e893fb2
Joshua Carp <jm.carp@gmail.com>
2013-07-14 18:56:39 -0400

    cleanup

--- js-src/main.js
+++ js-src/main.js
@@ -1,5 +1,10 @@
-var defer = citelet.scrape();
+/*var defer = citelet.scrape();
 defer.done(function(data) {
+    citelet.send(data, {
+        source : 'bookmarklet'
+    });
+});*/
+citelet.scrape().done(function(data) {
     citelet.send(data, {
         source : 'bookmarklet'
     });
