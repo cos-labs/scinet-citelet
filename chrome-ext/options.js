ce79cea224c270e15feb2de73535ec733f311fc9
Joshua Carp <jm.carp@gmail.com>
2013-07-14 18:57:59 -0400

    revamped options

--- chrome-ext/options.js
+++ chrome-ext/options.js
@@ -1,20 +1,41 @@
 // Initialize mode <select> to currently stored value
-chrome.storage.local.get('mode', function(result) {
-    $('#mode').val(result.mode);
-});
+function get_mode() {
+    chrome.storage.local.get('mode', function(result) {
+        $('#mode').val(result.mode);
+    });
+}
+
+// Show progress
+function get_progress() {
+    chrome.storage.local.get('sent', function(stored) {
+        var count = 'sent' in stored ? Object.keys(stored.sent).length : 0;
+        $('#progress').text(count + ' articles contributed so far.');
+    });
+}
+
+function init() {
+    get_mode();
+    get_progress();
+}
+
+//
+init();
+
+// Set up event handlers
 
 // Save mode in Chrome storage
 $('#submit').click(function() {
     chrome.storage.local.set({mode : $('#mode').val()});
 });
 
-// Clear Chrome storage
-$('#clear').click(function() {
-    chrome.storage.local.clear();
+// Refresh options data
+$('#refresh').click(function() {
+    init();
 });
 
-// Show progress
-chrome.storage.local.get('sent', function(stored) {
-    var count = 'sent' in stored ? Object.keys(stored.sent).length : 0;
-    $('#progress').text(count + ' articles contributed so far.');
+// Clear Chrome storage
+$('#clear').click(function() {
+    chrome.storage.local.clear(function() {
+        init();
+    });
 });
