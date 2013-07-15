c9adcbd0618c0adff474a84e98d1e0a823728c18
root <jm.carp@gmail.com>
2013-07-15 14:18:27 +0000

    local -> sudo + cd

--- fabfile.py
+++ fabfile.py
@@ -2,16 +2,25 @@
 """
 
 # Imports
+import os
+from fabric.api import env
+from fabric.api import sudo
 from fabric.api import local
 from fabric.api import execute
+from fabric.context_managers import cd
+
+env.hosts = ['localhost']
+env.user = 'jmcarp'
+env.path = '/vol/citelet/'
 
 def _runall(mode):
     
-    execute(app_cfg, mode)
-    execute(build_js_cfg, mode)
-    execute(build_bookmarklet_js, mode)
-    execute(build_chrome_js, mode)
-    execute(build_chrome_manifest, mode)
+    with cd(env.path):
+        execute(app_cfg, mode)
+        execute(build_js_cfg, mode)
+        execute(build_bookmarklet_js, mode)
+        execute(build_chrome_js, mode)
+        execute(build_chrome_manifest, mode)
 
 def clean():
     """ Run all tasks in clean mode. """
@@ -28,18 +37,18 @@ def app_cfg(mode):
     
     if mode == 'deploy':
 
-        local('cp cfg.py app/citelet_cfg.py')
+        sudo('cp cfg.py app/citelet_cfg.py')
 
     elif mode == 'clean':
         
-        local('rm app/citelet_cfg.py')
+        sudo('rm app/citelet_cfg.py')
 
 def build_js_cfg(mode):
     """ Create JavaScript config file. """
     
     if mode == 'deploy':
 
-        local('''
+        sudo('''
             python py-util/render.py \
                 --i js-cfg/cfg.js.jinja \
                 --o js-cfg/cfg.js
@@ -47,14 +56,14 @@ def build_js_cfg(mode):
 
     elif mode == 'clean':
         
-        local('rm js-cfg/cfg.js')
+        sudo('rm js-cfg/cfg.js')
 
 def build_chrome_manifest(mode):
     """ Render Chrome manifest. """
     
     if mode == 'deploy':
 
-        local('''
+        sudo('''
             python py-util/render.py \
                 --i chrome-ext/manifest.json.jinja \
                 --o chrome-ext/manifest.json
@@ -62,14 +71,14 @@ def build_chrome_manifest(mode):
 
     elif mode == 'clean':
         
-        local('rm chrome-ext/manifest.json')
+        sudo('rm chrome-ext/manifest.json')
 
 def build_bookmarklet_js(mode):
     """ Minify bookmarklet JavaScript. """
     
     if mode == 'deploy':
 
-        local('''
+        sudo('''
             python py-util/minify.py \
                 --wrap \
                 --minify \
@@ -84,14 +93,14 @@ def build_bookmarklet_js(mode):
 
     elif mode == 'clean':
         
-        local('rm app/static/js/citelet.min.js')
+        sudo('rm app/static/js/citelet.min.js')
 
 def build_chrome_js(mode):
     """ Minify Chrome JavaScript. """
     
     if mode == 'deploy':
 
-        local('''
+        sudo('''
             python py-util/minify.py \
                 --files \
                     js-cfg/*.js \
@@ -103,4 +112,4 @@ def build_chrome_js(mode):
 
     elif mode == 'clean':
         
-        local('rm chrome-ext/citelet.min.js')
+        sudo('rm chrome-ext/citelet.min.js')
